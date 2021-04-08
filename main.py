82164db96df9edd5f2b0b64910f762951aa66917
br-wa <brwa@mit.edu>
2021-04-08 16:50:17 -0400

    initial setup

---
+++ main.py
@@ -0,0 +1,77 @@
+import os
+import platform
+import random
+import shutil
+import sys
+import functools
+import itertools as it
+import copy
+
+import numpy as np
+import pandas as pd
+import sklearn.metrics
+import h5py
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+import tensorflow.keras as K
+
+training_set = None #should have shape [n_TFs, n_cells, n_peaks]
+training_labels = None
+test_set = None
+test_labels = None
+
+#hyperparams, will tune eventually
+num_epochs = 25
+batch_size = 100
+learning_rate = 0.005
+
+def get_data():
+    #TODO load and process data  
+    data_dir = os.path.join('.', None) #TODO
+
+def initialize_A():
+    #defining A as K.sequential()
+
+    return A
+
+def initialize_B():
+    #defining B as K.sequential()
+
+    return B
+
+def initialize_C():
+    #defining C as K.sequential()
+
+    return C
+
+def loss(predictions, y):
+    #defining loss function, probably just MSE for now?
+
+def train_step(A, B, C, x, y, optimizer):
+    with tf.gradientTape() as tape:
+        predictions = C(tf.concat(A(x[:,0:blah]), B(x[:,blah+1:])), axis=0) #TODO set blah
+        pass_loss = loss(predictions, y)
+    A_gradients = tape.gradient(pass_loss, A.trainable_variables)
+    B_gradients = tape.gradient(pass_loss, B.trainable_variables)
+    C_gradients = tape.gradient(pass_loss, C.trainable_variables)
+    optimizer.apply_gradients(zip(A_gradients, A.trainable_variables))
+    optimizer.apply_gradients(zip(B_gradients, B.trainable_variables))
+    optimizer.apply_gradients(zip(C_gradients, C.trainable_variables))
+    
+    return pass_loss
+
+def train(A, B, C):
+    optimizer = tf.optimizers.RMSprop(learning_rate=learning_rate, rho=0.9)
+    for _ in range(num_epochs):
+        
+
+
+def main():
+    get_data()
+    A = initialize_A()
+    B = initialize_B()
+    C = initialize_C()
+    
